986b5c4c8edaa92107a639c9430643545f5690f0
Bruno Garcia Moura <bgarciamoura@gmail.com>
2022-05-07 18:36:28 -0300

    feat: Updating a task

--- src/interfaces/ITarefa.ts
+++ src/interfaces/ITarefa.ts
@@ -1,6 +1,7 @@
 import IProjeto from './IProjeto';
 
 interface ITarefa {
+	id: number;
 	descricao: string;
 	duracaoEmSegundos: number;
 	projeto: IProjeto;
--- src/views/Tarefas/index.vue
+++ src/views/Tarefas/index.vue
@@ -5,10 +5,64 @@
 			v-for="(tarefa, index) in tarefas"
 			:key="index"
 			:tarefa="tarefa"
+			@aoTarefaClicada="selecionarTarefa"
 		/>
 		<box v-if="listaEstaVazia"
 			>Você não está muito produtivo hoje! 🙁</box
 		>
+	</div>
+	<div
+		class="modal"
+		:class="{ 'is-active': tarefaSelecionada }"
+		v-if="tarefaSelecionada"
+	>
+		<div class="modal-background"></div>
+		<div class="modal-card">
+			<header class="modal-card-head">
+				<p class="modal-card-title">
+					Editando a tarefa
+					{{
+						tarefaSelecionada?.descricao ||
+						'Tarefa sem descrição'
+					}}
+				</p>
+				<button
+					class="delete"
+					aria-label="close"
+					@click="fecharModal"
+				></button>
+			</header>
+			<section class="modal-card-body">
+				<div class="field">
+					<label
+						for="descricaoDaTarefa"
+						class="label"
+					>
+						Descricao
+					</label>
+					<input
+						type="text"
+						id="descricaoDaTarefa"
+						class="input"
+						v-model="tarefaSelecionada.descricao"
+					/>
+				</div>
+			</section>
+			<footer class="modal-card-foot">
+				<button
+					class="button is-success"
+					@click="alterarTarefa"
+				>
+					Salvar Alterações
+				</button>
+				<button
+					@click="fecharModal"
+					class="button"
+				>
+					Cancelar
+				</button>
+			</footer>
+		</div>
 	</div>
 </template>
 
@@ -21,6 +75,7 @@
 	import { ITarefa } from '@/interfaces/ITarefa';
 	import { useStore } from '@/store';
 	import {
+		ALTERAR_TAREFA,
 		CADASTRAR_TAREFA,
 		OBTER_PROJETOS,
 		OBTER_TAREFAS,
@@ -43,6 +98,11 @@
 				tarefas,
 			};
 		},
+		data() {
+			return {
+				tarefaSelecionada: null as null | ITarefa,
+			};
+		},
 		computed: {
 			listaEstaVazia(): boolean {
 				return this.tarefas.length === 0;
@@ -51,6 +111,19 @@
 		methods: {
 			salvarTarefa(tarefa: ITarefa): void {
 				this.store.dispatch(CADASTRAR_TAREFA, tarefa);
+			},
+			selecionarTarefa(tarefa: ITarefa): void {
+				this.tarefaSelecionada = tarefa;
+			},
+			fecharModal(): void {
+				this.tarefaSelecionada = null;
+			},
+			alterarTarefa(): void {
+				this.store
+					.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+					.then(() => {
+						this.fecharModal();
+					});
 			},
 		},
 	});
